a424da02440ece68b0e538ba5216800c8813b4f6
shihabbabu420 <mdshihabbabu478@gmail.com>
2026-04-15 20:15:29 +0600

    aboutFriend component added

--- src/components/FriendsInfo/AboutFriend.jsx
+++ src/components/FriendsInfo/AboutFriend.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { IoMdArchive } from "react-icons/io";
+import { IoNotificationsCircle } from "react-icons/io5";
+import { MdDeleteForever } from "react-icons/md";
 
-const AboutFriend = () => {
-    return (
-        <div>
-            <h2>about friend</h2>
+const AboutFriend = ({ expectedFriend }) => {
+  const { tags, status, picture, name, bio } = expectedFriend;
+  return (
+    <div className="space-y-5 my-10">
+      <div className="shadow-lg mx-auto w-full bg-white p-4 flex flex-col justify-center items-center gap-1 rounded-lg">
+        <img
+          className="h-24 w-24 rounded-full object-cover"
+          src={picture}
+          alt={name}
+        />
+        <h4 className="text-2xl font-semibold text-gray-800">{name}</h4>
+        <p className="text-gray-400">62d ago</p>
+        <div
+          className={`"border px-3 py-0.5 rounded-full capitalize " ${status == "overdue" && "bg-red-700 "} ${status == "on_track" && "bg-green-900"} ${status == "almost_due" && "bg-yellow-600"}`}
+        >
+          <p className="text-white">{status.split("_").join(" ")}</p>
         </div>
-    );
+        <div className="flex gap-2 mb-1">
+          {tags.map((tag, idx) => (
+            <div
+              className="px-3 py-0.5 rounded-full capitalize bg-green-400 text-white "
+              key={idx}
+            >
+              {tag}
+            </div>
+          ))}
+        </div>
+        <p className="text-gray-600 font-semibold text-lg text-center">
+          <i>"{bio}"</i>
+        </p>
+        <p className="text-sm text-gray-600 font-semibold">Preferred: email</p>
+      </div>
+      <div className="space-y-3">
+        <button className="btn w-full bg-white text-gray-600"><IoNotificationsCircle className="text-lg" />Snooze 2 weeks</button><button className="btn w-full bg-white text-gray-600"><IoMdArchive className="text-base" />Archive</button><button className="btn w-full bg-white text-red-400"><MdDeleteForever className="text-xl" />Delete</button>
+      </div>
+    </div>
+  );
 };
 
 export default AboutFriend;
--- src/pages/details/page.jsx
+++ src/pages/details/page.jsx
@@ -1,19 +1,31 @@
-import React, { useContext } from 'react';
-import FriendInfo from '../../components/FriendsInfo/FriendInfo';
-import AboutFriend from '../../components/FriendsInfo/AboutFriend';
-import useFriendsData from '../../hooks/useFriendsData';
-import { FriendsDataContext } from '../../context/FriendsDataContext';
+import React, { useContext } from "react";
+import FriendInfo from "../../components/FriendsInfo/FriendInfo";
+import AboutFriend from "../../components/FriendsInfo/AboutFriend";
+import useFriendsData from "../../hooks/useFriendsData";
+import { FriendsDataContext } from "../../context/FriendsDataContext";
+import { useParams } from "react-router";
+import { FadeLoader } from "react-spinners";
 
 const FriendDetails = () => {
-    const {friendsData, isLoading} = useFriendsData()
-    const checkInData = useContext(FriendsDataContext)
-    console.log(checkInData);
-    return (
-        <div className='w-[95%] md:w-[90%] lg:w-[85%] mx-auto space-y-5 h-[60vh] border grid grid-cols-1 lg:grid-cols-4'>
-            <div><AboutFriend></AboutFriend></div>
-            <div className='col-span-3'><FriendInfo></FriendInfo></div>
+  const { friendsData, isLoading } = useFriendsData();
+  const { id } = useParams();
+  const checkInData = useContext(FriendsDataContext);
+  const expectedFriend = friendsData.find((friend) => friend.id == id);
+  // console.log(expectedFriend);
+  return (
+    <div className="w-[95%] md:w-[90%] lg:w-[85%] mx-auto space-y-5 ">
+      {isLoading ? (
+        <div className="min-h-[60vh] flex justify-center items-center">
+          <FadeLoader color="#01811f"></FadeLoader>
         </div>
-    );
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-4">
+          <AboutFriend expectedFriend={expectedFriend}></AboutFriend>
+          <FriendInfo expectedFriend={expectedFriend}></FriendInfo>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FriendDetails;
