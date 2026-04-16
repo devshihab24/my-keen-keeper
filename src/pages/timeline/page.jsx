5c914a84338f27eb450352e3c87fc26383885d35
shihabbabu420 <mdshihabbabu478@gmail.com>
2026-04-16 19:47:08 +0600

    timeline filter functionality added

--- src/pages/timeline/page.jsx
+++ src/pages/timeline/page.jsx
@@ -1,24 +1,40 @@
-import React, { Activity, useContext } from "react";
+import React, { Activity, useContext, useState } from "react";
 import { ActivityContext } from "../../context/ActivityContext";
 import FriendActivity from "../../components/friendActivity/FriendActivity";
 
 const TimeLine = () => {
   const { activity } = useContext(ActivityContext);
-  console.log(activity);
+  const [filterActivity, setFilterActivity] = useState(activity);
+  // console.log(activity);
+  const handleFilter = (e) => {
+    console.log(e.target.value);
+    if (e.target.value == "all") {
+      return setFilterActivity(activity);
+    }
+    const filteredData = activity.filter(
+      (data) => data.a_type == e.target.value,
+    );
+    setFilterActivity(filteredData);
+  };
+  console.log(filterActivity);
   return (
     <div className="py-10 w-[95%] md:w-[90%] lg:w[85%] mx-auto space-y-5">
       <div className="space-y-4">
         <h2 className="text-4xl font-bold text-gray-800">Timeline </h2>
-        <select className="w-80 border px-2 py-1 rounded" >
-          <option value="call">call</option>
-          <option value="text">text</option>
-          <option value="video">video</option>
+        <select
+          onChange={handleFilter}
+          className="w-80 border px-2 py-1 rounded"
+        >
+          <option value="all">All</option>
+          <option value="call">Call</option>
+          <option value="text">Text</option>
+          <option value="video">Video</option>
         </select>
       </div>
       <div className="space-y-3">
-        {
-            activity.map((item) => <FriendActivity key={item.a_id} item={item}></FriendActivity>)
-        }
+        {filterActivity.map((item, idx) => (
+          <FriendActivity key={idx} item={item}></FriendActivity>
+        ))}
       </div>
     </div>
   );
